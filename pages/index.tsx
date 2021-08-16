59fc9a8d0f93c5b13a7a12d764c3de34be30b4f2
r3yohei <r3yohei@gmail.com>
2021-08-16 15:12:07 +0900

    チュートリアルのts化

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home({
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <u1 className={utilStyles.list}>
+        <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
@@ -50,7 +50,7 @@ export default function Home({
               </small>
             </li>
           ))}
-        </u1>
+        </ul>
       </section>
     </Layout>
   )
